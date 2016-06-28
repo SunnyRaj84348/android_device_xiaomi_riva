3f63ef71cc479aceecbe606e037b8f6d90635349
Manikanta Kanamarlapudi <kmanikan@codeaurora.org>
2016-06-28 19:19:04 +0530

    land: Disable split mode
    Disable split mode via setprop
    vidc.disable.split.mode=1
    
    Change-Id: Ia5e88721760990187a9371ab1b705bff7a37de50

--- product/media.mk
+++ product/media.mk
@@ -12,4 +12,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     mm.enable.smoothstreaming=true \
     media.aac_51_output_enabled=true \
     vidc.enc.narrow.searchrange=1 \
+    vidc.disable.split.mode=1 \
     drm.service.enabled=true

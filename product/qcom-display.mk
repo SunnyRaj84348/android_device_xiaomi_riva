6cd12e10a82897a6d7bac73fcbbc2c8c64afbe01
Ananta Kishore K <akollipa@codeaurora.org>
2016-07-01 12:33:30 +0530

    land: Add vulkan features
    Vulkan hardware level & API version features
    are required to enable Vulkan Android CTS tests
    execution.
    
    Change-Id: Id75826c8f1d9528f8e40fe24109b5a9ded30043b

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -15,3 +15,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \
     persist.hwc.mdpcomp.enable=true
+
+# Vulkan
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml

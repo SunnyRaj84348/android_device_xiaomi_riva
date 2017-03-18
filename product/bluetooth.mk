108fe4fc3e1bc31017ec2c776980acc5eed2f98f
karthick111 <karthickrko61@gmail.com>
2017-03-18 05:05:50 +0100

    land: Set qcom bluetooth soc ID
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -5,6 +5,7 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.bluetooth.soc=smd \
     ro.bluetooth.dun=true \
     ro.bluetooth.hfp.ver=1.7 \
     ro.bluetooth.sap=true \

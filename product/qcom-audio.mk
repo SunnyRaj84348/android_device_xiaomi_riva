f85ab48d7858389035aa75fcba733daf254a30cf
karthick111 <karthickrko61@gmail.com>
2017-03-19 13:16:22 +0100

    Revert "land: Remove prebuilt audio HAL"
    This reverts commit 85a64b41547ee5b1921ca9763c6c82695ef50626.

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -3,7 +3,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
 PRODUCT_PACKAGES += \
-    audio.primary.$(TARGET_BOARD_PLATFORM) \
     audiod \
     libqcompostprocbundle \
     libqcomvisualizer \

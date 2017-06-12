0520f1836afb4eb41cb0976128d190f81412161b
Sanjay Singh <sisanj@codeaurora.org>
2017-06-12 14:23:13 +0530

    msm8917-common: changing property name
    Changing property name with vendor prefix.
    
    Change-Id: I98e0108a3b39746a8957631adc4bbd4599cc17b1

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,11 +126,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     mm.enable.qcom_parser=4176895 \
     media.stagefright.audio.sink=280 \
+    vendor.vidc.disable.split.mode=1 \
     vendor.vidc.enc.disable_bframes=1 \
     vendor.vidc.enc.disable_pframes=1 \
-    vendor.vidc.enc.narrow.searchrange=1 \
-    vendor.vidc.disable.split.mode=1 \
-    vidc.enc.disable.pq=true
+    vendor.vidc.enc.disable.pq=true \
+    vendor.vidc.enc.narrow.searchrange=1
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \

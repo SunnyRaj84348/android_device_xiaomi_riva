88634ecd381a74a83790d9418491e8801e21733d
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-06-26 15:03:08 +0200

    land: Disable corectl & cpusets
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -5,8 +5,6 @@ PRODUCT_PACKAGES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.am.reschedule_service=true \
-    ro.core_ctl_min_cpu=2 \
-    ro.core_ctl_max_cpu=4 \
     ro.vendor.at_library=libqti-at.so \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.gt_library=libqti-gt.so

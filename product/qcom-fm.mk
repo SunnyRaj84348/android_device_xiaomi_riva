c8b20cfe02035862bd5baddc27a74c23af566b43
karthick111 <karthickrko61@gmail.com>
2017-06-27 11:43:37 +0200

    land: Update FM configs

--- product/qcom-fm.mk
+++ product/qcom-fm.mk
@@ -1,13 +1,4 @@
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
-# Init
-PRODUCT_PACKAGES += \
-    init.qcom.fm.sh
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.fm.transmitter=false
+    FMRadio \
+    libfmjni

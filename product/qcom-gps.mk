760ab73a1260c9c4bda54332dd8170039c36d81b
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-07-14 19:03:57 +0200

    land: Update GPS from garlic
    * Revision: 17b6a1f56bdda4d1c4bfd0ad514fe97bdd5ca810
    
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,11 +1,18 @@
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8937 \
-    gps.conf \
     libcurl
 
 PRODUCT_BOOT_JARS += \
     com.qti.location.sdk
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
 # Permissions
 PRODUCT_COPY_FILES += \

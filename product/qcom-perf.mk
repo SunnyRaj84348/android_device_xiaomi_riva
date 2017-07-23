a63bb106bffee5ce27da36c95cde96263ffc222d
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-07-23 15:34:21 +0200

    land: Clean up unused stuffs
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -5,6 +5,4 @@ PRODUCT_PACKAGES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.am.reschedule_service=true \
-    ro.vendor.at_library=libqti-at.so \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.gt_library=libqti-gt.so
+    ro.vendor.extension_library=libqti-perfd-client.so

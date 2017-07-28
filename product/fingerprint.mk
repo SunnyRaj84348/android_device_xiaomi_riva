8ee9910310b3bea4c126dcc1a2590d6197ea4995
karthick111 <karthickrko61@gmail.com>
2017-07-28 14:04:15 +0200

    land: Releasetool extension to remove fp blobs

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -5,3 +5,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
+# Non-fp patch
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/check_features.sh:install/bin/check_features.sh

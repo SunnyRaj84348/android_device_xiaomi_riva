1ccfb5a469493c896dd1966612d1df885220ca5a
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-08-07 15:41:46 +0200

    land: Add button jack keylayout
    * for msm8952-snd-card-sku1.
    
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,4 +1,5 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/ft5336_ts.kl:system/usr/keylayout/ft5336_ts.kl \
-    $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+    $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(DEVICE_PATH)/keylayout/msm8952-snd-card-sku1_Button_Jack.kl:system/usr/keylayout/msm8952-snd-card-sku1_Button_Jack.kl

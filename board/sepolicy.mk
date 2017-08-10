a85558ea5273dd5bca7d9404fc05d3947450a81e
Martijn Coenen <maco@google.com>
2017-08-10 17:15:00 +0200

    msm8937-common: Add selinux/private/service_contexts.
    This makes sure that the remaining binder services
    for radio end up in plat_service_contexts.
    
    That in turn allows us to enforce that servicemanager
    will only serve services from plat_service_contexts
    on FULL_TREBLE devices.
    
    Bug: 36866029
    Test: boot, verify radio services still work
    Change-Id: Ib67b3a03e5599484c5c4fb27a0f323a37dd51636
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- board/sepolicy.mk
+++ board/sepolicy.mk
@@ -2,4 +2,7 @@ include device/qcom/sepolicy/sepolicy.mk
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-    $(VENDOR_PATH)/sepolicy
+    $(VENDOR_PATH)/sepolicy/vendor
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(VENDOR_PATH)/sepolicy/private

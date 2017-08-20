371b2f3fea806566797b8950d0fac6de15143988
karthick111 <karthickrko61@gmail.com>
2017-08-20 11:42:27 +0200

    land: Cleanup configurations

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
     libshims_camera \
     libshim_ims_camera
 
-# ZRAM
+# Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
     media.camera.ts.monotonic=0 \

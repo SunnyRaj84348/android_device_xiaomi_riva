fdb673e3108121aa20fdab83ae0c1b1341ebc980
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    msm8937-common: Build Display HIDL HAL interfaces
     * Enable binderized Gralloc HAL along with it
     * Enable HWC2
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- product/display.mk
+++ product/display.mk
@@ -19,6 +19,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjni_livedisplay
 
+# Display HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml

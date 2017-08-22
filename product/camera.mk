6d42ff1b7ea72b72f9ee7e6f176818ea09a2069e
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    msm8937-common: Build Camera HIDL HAL interface and provider
    Change-Id: Id845e815310027d0feb60c83b487f776d9175fa1

--- product/camera.mk
+++ product/camera.mk
@@ -4,3 +4,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
+
+# HIDL Packages
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl

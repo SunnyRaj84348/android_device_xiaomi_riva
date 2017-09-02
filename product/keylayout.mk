5916b63dee376e544b487f27a706ce74e7925136
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-09-03 01:13:25 +0800

    land: Update headphone button layout
    * As per latest MIUI Global Beta.
    * Thanks to danielstuart14.
    
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
    Signed-off-by: charansai612 <charansai612@gmail.com>

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/ft5336_ts.kl:system/usr/keylayout/ft5336_ts.kl \
     $(DEVICE_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(DEVICE_PATH)/keylayout/msm8952-snd-card-sku1_Button_Jack.kl:system/usr/keylayout/msm8952-snd-card-sku1_Button_Jack.kl \
+    $(DEVICE_PATH)/keylayout/msm8952-sku1-snd-card_Button_Jack.kl:system/usr/keylayout/msm8952-sku1-snd-card_Button_Jack.kl \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
 
 # IDC

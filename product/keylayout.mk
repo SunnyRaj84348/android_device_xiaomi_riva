1c0752b2fa80a818e726d844ab9c02e8cbcb1c8f
nervehammer <nervehammer.suraj@gmail.com>
2017-10-13 13:23:24 +0000

    land: Add keylayout for Redmi 3X (Imagis touchscreen)
    Change-Id: I3ab0017f0d12a6b4adcdc9205149aa24b2136603

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,6 +1,7 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/ft5x06_720p.kl:system/usr/keylayout/ft5x06_720p.kl \
+    $(DEVICE_PATH)/keylayout/ist30xx_ts_input.kl:system/usr/keylayout/ist30xx_ts_input.kl \
     $(DEVICE_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(DEVICE_PATH)/keylayout/msm8952-sku1-snd-card_Button_Jack.kl:system/usr/keylayout/msm8952-sku1-snd-card_Button_Jack.kl \

734163a4a0a08e6ebd5ddecc4a8e0fef9b2eb903
Bharath Gopal <gopalb@codeaurora.org>
2017-11-03 12:29:29 +0530

    msm8937-common: Add property to enable display default color mode
     Add display default color mode property with value of 1.
    
    Change-Id: I89ffc625dbb0fa731dd0d6ed5aa5de4ec97e2f36

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -67,7 +67,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=2 \
-    ro.sf.lcd_density=320
+    ro.sf.lcd_density=320 \
+    vendor.display.enable_default_color_mode=1
 
 # Fm
 PRODUCT_PROPERTY_OVERRIDES += \

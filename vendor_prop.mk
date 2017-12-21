3bcfab55a1ce9ccc922a02551eed6d68dd6b85a0
Baldev Sahu <bsahu@codeaurora.org>
2017-12-21 10:41:59 +0530

    msm8937-common: Disable debug.sf.recomputecrop
    Disable recompute crop change in SF
    
    Change-Id: Ifd5bae734ccc4962427a64f789cf9890f10f5965

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -83,6 +83,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
+    debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \

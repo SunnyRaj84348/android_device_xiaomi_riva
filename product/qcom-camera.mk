2c189b3815befad94e004d621e617fcfc33e15bc
Prateek Chaubey <chaubeyprateek@gmail.com>
2017-12-23 23:43:46 +0530

    land: update Camera props * Kick Broken Ones * Disable HAL3 * Fixes Recording With 3rd Party Apps

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -7,10 +7,11 @@ PRODUCT_PACKAGES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    camera.display.umax=1920x1080 \
+    camera.display.lmax=1280x720 \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
     media.camera.ts.monotonic=0 \
     persist.camera.gyro.android=1 \
+    persist.camera.HAL3.enabled=0 \
     persist.camera.is_type=1 \
-    vidc.debug.perf.mode=2 \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    vidc.enc.disable.pq=true
+    vidc.enc.narrow.searchrange=1

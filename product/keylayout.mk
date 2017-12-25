60dee10c119303bb7e8e2e1d3d3f74a3cc3e7038
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-12-25 11:21:35 +0530

    land: Rename keylayout

--- product/keylayout.mk
+++ product/keylayout.mk
@@ -1,6 +1,6 @@
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/keylayout/ft5336_ts.kl:system/usr/keylayout/ft5336_ts.kl \
+    $(DEVICE_PATH)/keylayout/ft5x06_720p.kl:system/usr/keylayout/ft5x06_720p.kl \
     $(DEVICE_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(DEVICE_PATH)/keylayout/msm8952-sku1-snd-card_Button_Jack.kl:system/usr/keylayout/msm8952-sku1-snd-card_Button_Jack.kl \

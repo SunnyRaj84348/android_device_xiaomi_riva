8e4add1f91e14ae0321ffdf914e135990dbd0925
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    msm8917-common: Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Change-Id: I256bae45bc3c68febe5d7bb4bdbd559d752b7d96

--- msm8917.mk
+++ msm8917.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \

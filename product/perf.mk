d1f2c50dd85cd30da933d9a9e91f055dbb1dd967
Bruno Martins <bgcngm@gmail.com>
2018-01-13 11:46:35 +0000

    msm8937-common: Update perf stack to O
     * From: TA-1025_00WW 8.1.0 OPR1.170623.026

--- product/perf.mk
+++ product/perf.mk
@@ -1,4 +1,7 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.am.reschedule_service=true \
-    ro.sys.fw.use_trim_settings=true
+    ro.vendor.qti.sys.fw.use_trim_settings=true \
+    ro.vendor.qti.sys.fw.empty_app_percent=50 \
+    ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+    ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+    ro.vendor.qti.sys.fw.trim_enable_memory=6442450944
--- product/power.mk
+++ product/power.mk
@@ -1,3 +1,4 @@
 # Power IMPL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service

8dc80296b34efcb068bd640dde2c2cca47aea2a0
Prateek Chaubey <chaubeyprateek@gmail.com>
2018-01-21 08:20:43 +0000

    msm8937-common: Build Consumerir HIDL HAL interfaces

--- product/consumerir.mk
+++ product/consumerir.mk
@@ -1,3 +1,7 @@
+# Consumerir HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml

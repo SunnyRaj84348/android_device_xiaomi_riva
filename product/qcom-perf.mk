852bf8a7c916888fa7c149e39d140d845672ebfa
oshmoun <oshmoun100@googlemail.com>
2018-02-18 23:00:04 +0100

    msm8937-common: Add public.libraries.txt
    Android needs this text file to know what libraries are allowed to be loaded by apps directly.
    For now, add:
    libOpenCL.so to the list, in order to get OpenCL to function properly.
    adsprpc related libraries, since they were also declared public in stock.
    
    Change-Id: I15ef7a7ab7d1f1a0cc6b484cd2ca9993f40de23b

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -2,6 +2,10 @@
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so

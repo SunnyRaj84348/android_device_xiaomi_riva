8510cde8a94742f05395f411515d2070d59139f0
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    riva: Build LiveDisplay HAL
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- device.mk
+++ device.mk
@@ -122,7 +122,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8937 \
     libdisplayconfig \
     libqdMetaData.system \
-    libjni_livedisplay \
     libtinyxml \
     memtrack.msm8937
 
@@ -218,6 +217,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
+
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \

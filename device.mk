87fd39199eec173393cf6e05717cdee9d08992df
Shubham Das <dasshubham762@gmail.com>
2018-02-26 18:37:54 +0530

    riva: Add MIUI Poweroff Charging Animation
    Surprisingly it is working very well while offline charging, same as MIUI
    
    Reference- https://review.lineageos.org/#/c/205385/
    
    Change-Id: I1282a368bc9b0b9cd51cbbcde8d9973dce822de6

--- device.mk
+++ device.mk
@@ -109,6 +109,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Charger
+include $(LOCAL_PATH)/rootdir/charger/charger.mk
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8937 \

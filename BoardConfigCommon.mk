9b2c3642e8388ee7730339f87fef5d5ec69c7d09
Vinay Verma <vvinay@codeaurora.org>
2018-04-16 07:56:07 +0530

    msm8917-common: Add MATRIX file for vendor HAL's
    Change-Id: I6c7eed54f066e61ad8039eef5ee6081e6de43560

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,6 +110,7 @@ TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(VENDOR_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml

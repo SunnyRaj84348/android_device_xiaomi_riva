03c59db4ccddf1ad9c8ba42b6a05221339b64e73
Offain <fabian.leutenegger@bluewin.ch>
2018-04-18 20:19:29 +0200

    msm8937-common: Fix unknown baseband in settings for more devices
     * Otherwise it wont work on Redmi 4A and Redmi 5A
    
    Change-Id: I3995d83d066cbd30e2250cce2445469960dccb92

--- rootdir/init.qcom.baseband.sh
+++ rootdir/init.qcom.baseband.sh
@@ -27,4 +27,4 @@
 #
 
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /dev/block/bootdevice/by-name/modem | grep "^MPSS.TA." | head -1`
+setprop gsm.version.baseband `strings /dev/block/bootdevice/by-name/modem | grep "^MPSS.*_PACK" | head -1`

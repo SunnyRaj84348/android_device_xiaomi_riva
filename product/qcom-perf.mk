7fcaa86e0fabe6e069ec2d9147ee0f5c2ca8e2da
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 00:49:11 +0300

    msm8937-common: Add powerhint.xml
     * Based on LA.UM.6.6.r1-07400-89xx.0, adapted for the new format.
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ief38dc242a92de05305452634fe2318e4de42f90

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -1,3 +1,7 @@
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so

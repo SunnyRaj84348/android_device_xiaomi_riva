18ed322ef375ead2c69be729bf85160a75fc53ed
Isaac Chen <isaacchen@isaacchen.cn>
2018-07-01 13:54:19 +0200

    msm8937-common: Cleanup GPS makefiles and configs
    * Update configs from bardock 8.1.0 OPM1.171019.019
    * Cleanup unused blobs

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,12 +1,12 @@
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
     libcurl \
     libgnss \
     libgnsspps
 
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/gps/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
     $(VENDOR_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(VENDOR_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(VENDOR_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \

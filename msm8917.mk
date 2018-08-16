d331bc50290433fd2f21eac99d05bd94452ba22b
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    msm8917-common: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- msm8917.mk
+++ msm8917.mk
@@ -343,6 +343,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \

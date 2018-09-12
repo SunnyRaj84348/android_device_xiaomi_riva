2a58bbb65d3ad7f236fe00230eab13dc3f75f461
Nich <nctrenco@gmail.com>
2018-09-13 01:02:11 +0800

    msm8017-common: vendor_prop: Set virtual displays to 0
    We don't have any virtual displays, however HWC assumes 1 virtual
    display by default. This was causing laggy display when allocating
    virtual displays, as done by surfaceflinger during screen recording
    for example.
    
    Change-Id: Iab7ddb4182ee1d952f3083942eece033f651af98

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -91,6 +91,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
+    debug.sdm.support_writeback=0 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.disable_vds=1 \

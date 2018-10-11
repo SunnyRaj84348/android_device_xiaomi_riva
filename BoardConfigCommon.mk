b5bd67765f3e3ee7f426b6953e91c20c43bf1a3f
Rashed Abdel-Tawab <rashed@linux.com>
2018-10-11 14:46:37 +0300

    msm8917-common: Ignore Selinux Neverallows
    Change-Id: I585ae6bac9d0ed303422f8dfa85ecf83932bb2cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,6 +188,11 @@ VENDOR_SECURITY_PATCH := 2019-03-01
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true

946ea49f74b17badcd95c39f356c79b2d4ecb691
dianlujitao <dianlujitao@lineageos.org>
2018-11-10 15:11:10 +0800

    msm8917-common: Update GPS blobs from daisy
     * From: xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Change-Id: Ia1646ae0b8158c1b312221788c08427a754f3a36

--- msm8917.mk
+++ msm8917.mk
@@ -143,7 +143,8 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     libgnss \
-    libgnsspps
+    libgnsspps \
+    libsensorndkbridge
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \

731ae66d94762f1a282b7f843acaa038160519c3
karthick111 <karthickrko61@gmail.com>
2018-12-19 00:37:59 +0530

    msm8937-common: Build thermal hidl hal
    Change-Id: I3819050f7d9d8cbaa2452cef8602d44da6337977

--- msm8937.mk
+++ msm8937.mk
@@ -347,6 +347,10 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Vibrator

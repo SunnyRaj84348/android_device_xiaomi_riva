68a2e1e17dfa03af10750d747df50a55192be367
Michael Bestas <mkbestas@lineageos.org>
2018-12-25 18:14:23 +0200

    msm8917-common: Update GPS to LA.UM.7.6.r1-03900-89xx.0
    Change-Id: Idcb69c00a6d4ebfebd23d97bf5c7d7b6794937f2

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 The LineageOS Project
+# Copyright 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,5 +15,6 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/build/target_specific_features.mk
 
 include $(call all-makefiles-under,$(LOCAL_PATH))

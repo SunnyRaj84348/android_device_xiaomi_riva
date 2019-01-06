567cef740cc9a039da2ca2d5e102d69ccfa788fc
Anay Wadhera <anay1018@gmail.com>
2019-01-05 20:23:43 -0600

    riva: don't build extra copies of telephony-ext since that is already in vendor/pa

--- device.mk
+++ device.mk
@@ -343,9 +343,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \

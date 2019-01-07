ce31587e9216a3e8bdd63574ad07291ad117e28b
Michael Bestas <mkbestas@lineageos.org>
2019-01-08 01:51:52 +0200

    msm8917-common: Change how we set the alarm property
    * We used to have an init extension, which breaks treble
      and since P it requires custom SELinux rules to allow
      init reading /proc/sys/kernel/boot_reason
    * Remove the init extension and set the property like
      QCOM does through init.qcom.early_boot.sh
    * Update the property with vendor prefix while we are at it
    
    Change-Id: I238f754a17c9a548c6c90503bfa4b21763d1b4c4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -51,6 +51,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := init.qcom.early_boot.sh
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS :=ETC
+LOCAL_SRC_FILES := etc/init.qcom.early_boot.sh
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC

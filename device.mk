ca61133ac8cb1e6d41fadcd86cf58cc3338e3841
Dante Russo <drusso@codeaurora.org>
2019-01-15 10:44:41 -0800

    riva: Add android.hardware.gnss@2.0 impl and service
    For building android.hardware.gnss@2.0-impl-qti
    and android.hardware.gnss@2.0-service-qti
    
    Change-Id: I52fd0615defa29b7df683a1f302339556e634e9e
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- device.mk
+++ device.mk
@@ -174,8 +174,8 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl
 
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \

e15063d614ee233f23a8791737a947326cc6e4ab
Michael Bestas <mkbestas@lineageos.org>
2019-01-19 21:17:27 +0200

    msm8917-common: Update bluetooth properties for pie
    Change-Id: I1697e91269fd7180e8d27456f658323a09a5d49f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,8 +48,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.bluetooth.soc=smd \
-    ro.qualcomm.bt.hci_transport=smd
+    vendor.qcom.bluetooth.soc=smd
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \

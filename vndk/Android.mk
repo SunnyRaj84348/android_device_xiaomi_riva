bd895131503570afc697f00fc134142c5c69e4b2
Michael Bestas <mkbestas@lineageos.org>
2019-01-19 20:27:10 +0200

    msm8917-common: Update perf stack from daisy
     * From: xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Change-Id: Iad7a2f52374d40761131796b03527b34297db9a6

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -35,8 +35,7 @@ VNDK_SP_LIBRARIES := \
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-    vendor.display.config@1.0 \
-    vendor.qti.hardware.iop@1.0
+    vendor.display.config@1.0
 
 INSTALL_IN_HW_SUBDIR := android.hidl.memory@1.0-impl
 

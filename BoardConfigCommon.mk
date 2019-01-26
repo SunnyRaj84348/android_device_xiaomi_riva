eda07824e8dd3a7699166a141a182c0c06e05d52
dianlujitao <dianlujitao@lineageos.org>
2019-01-26 20:28:26 +0800

    msm8917-common: Enable workaround for old MCC/MNC format
     * Fixes manual network selection
    
    Change-Id: I104a20491f4a6230c0d142db748e9545b22671a0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,6 +179,7 @@ TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 # RIL
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RIL_VARIANT := caf
+TARGET_USES_OLD_MNC_FORMAT := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk

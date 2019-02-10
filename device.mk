05c4351b43e6d5607c78a7b3deb33e909ae5f9af
Bruno Martins <bgcngm@gmail.com>
2019-02-10 17:59:14 +0000

    riva: Build Touch HAL to enable KeyDisabler
    Change-Id: I1dbe4626cda4a90d77e92ac971679473564c2fd8
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- device.mk
+++ device.mk
@@ -355,6 +355,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.riva
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service

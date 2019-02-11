58c18ea59ccf9c1a367390251573d206a87777ca
Murat Kozan <muratkozan350@gmail.com>
2019-02-11 16:57:13 +0300

    msm8917-common: Fix init.qcom.early_boot.sh file location
    Change-Id: I1c01429fd06d5fe83c8cf1a097982fa38eecb5ac

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -54,7 +54,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := init.qcom.early_boot.sh
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS :=ETC
-LOCAL_SRC_FILES := etc/init.qcom.early_boot.sh
+LOCAL_SRC_FILES := init.qcom.early_boot.sh
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 

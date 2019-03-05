51c477e935692e17d3a52c3e649425a069489746
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-03-05 10:57:05 +0000

    msm8937-common: Add riva support

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -40,7 +40,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "land santoni"
+write_headers "land santoni riva"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files-qc.txt true

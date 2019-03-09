2af5716a0ffcc5d478d04eaacf41f2e3f01f0f81
Rashed Abdel-Tawab <rashed@linux.com>
2019-03-09 11:14:06 -0800

    riva: Move privapp permissions to system
    These APKs live on system (since well, they're priv-app).
    Move these to system so they reside with their corresponding APKs.
    
    Change-Id: I454f4d07cb3916fab683fdb0078cdb0fab55f612

--- device.mk
+++ device.mk
@@ -287,7 +287,7 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+    $(DEVICE_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(DEVICE_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # RCS

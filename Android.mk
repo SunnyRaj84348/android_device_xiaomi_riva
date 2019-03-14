1d4341402afec8fa6f674ce43865ed69a2eb1ffc
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-03-14 07:33:39 +0000

    msm8917-common: Add rolex support

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter riva,$(TARGET_DEVICE)),)
+ifneq ($(filter riva rolex,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -40,7 +40,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "riva"
+write_headers "riva rolex"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files-qc.txt true

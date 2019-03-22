d8295bf622093f92aadcf053abf93bef75219f94
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-03-22 14:58:53 +0000

    riva: Correct path of qcom common sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,7 +182,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 VENDOR_SECURITY_PATCH := 2019-06-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/Android.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # We modify several neverallows, so let the build proceed

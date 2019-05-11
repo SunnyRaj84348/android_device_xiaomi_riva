61e16b25cec9329ef4defd3264de49eba7282d30
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-05-11 00:46:25 +0000

    msm8917-common: Drop init_msm8917

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,10 +112,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_msm8917
-TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8917
-
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000

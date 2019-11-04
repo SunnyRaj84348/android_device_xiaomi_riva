957a7cf3afa0b05869663d37c8d8d17c732d8f8c
SunnyRaj84348 <sunnyraj7461@gmail.com>
2019-11-04 16:01:59 +0530

    riva: Update dalvik properies for Q
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -71,11 +71,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
 # Display

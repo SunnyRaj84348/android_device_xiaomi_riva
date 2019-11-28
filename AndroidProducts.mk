441df730b43fe9104de49e7902cef76c9f39a8fd
Ahmad Rulim <mfalkia342@gmail.com>
2019-11-28 21:15:53 +0700

    riva: Update to aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_riva.mk
+    $(LOCAL_DIR)/aosp_riva.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,5 +21,5 @@
 # function: add_lunch_combo generic-eng
 
 for var in eng user userdebug; do
-  add_lunch_combo lineage_riva-$var
+  add_lunch_combo aosp_riva-$var
 done

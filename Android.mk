ba32d064079e47236c1cc458f4d97998352d9dfd
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-03-09 17:00:12 +0000

    riva: Update IMS symlink
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- Android.mk
+++ Android.mk
@@ -73,12 +73,12 @@ $(LOCAL_BUILT_MODULE):
 	$(hide) touch $@
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system/product/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 

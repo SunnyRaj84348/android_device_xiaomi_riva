18a937f6ee87bf49fe53ca8b2c69059020c2859a
ftatamn14 <66040459+ftatamn14@users.noreply.github.com>
2020-06-05 23:31:06 +0700

    Update and rename lineage_riva.mk to revengeos_riva.mk

--- revengeos_riva.mk
+++ revengeos_riva.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +30,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := revengeos_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 

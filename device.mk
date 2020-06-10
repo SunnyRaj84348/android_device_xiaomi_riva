247db7b7a4be55e84baf971e12ddee4c3fa505d4
winabs <66181505+winabs@users.noreply.github.com>
2020-06-10 23:16:37 +0700

    riva : update

--- device.mk
+++ device.mk
@@ -400,6 +400,8 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8937
+
+PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 

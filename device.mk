723d221030aee0122e68bc52d9c2abf0766a4fe7
Akmal <magchuz@pm.me>
2020-09-10 06:27:50 +0000

    riva: Add shim for libmmcamera2_stats_modules.so
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- device.mk
+++ device.mk
@@ -372,6 +372,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(DEVICE_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcamera_shim
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/riva

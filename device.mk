3c67caf442890429ebdcb85f1fdcfa083bb8cff8
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-09-17 02:51:18 +0000

    Revert "riva: Add libloc shim for gnss"
    This reverts commit 977ed97b67f6cef42078e780277bcd4ebaa26f7c.

--- device.mk
+++ device.mk
@@ -372,10 +372,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(DEVICE_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
-# Shims
-PRODUCT_PACKAGES += \
-    libloc_shim
-
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/riva

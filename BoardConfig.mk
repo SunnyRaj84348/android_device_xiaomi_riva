d43f1b47a26a049995bc510ece87a0bc1f9b8385
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-10-10 06:36:45 +0000

    riva: Match vendor security patch to blobs
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,7 +179,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-03-05
+VENDOR_SECURITY_PATCH := 2020-06-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
